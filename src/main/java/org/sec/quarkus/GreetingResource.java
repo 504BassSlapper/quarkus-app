55ca4485def9a4ccd8b56ad746163bde83dfda94
504BassSlapper <504bassslapper@gmail.com>
2023-05-09 12:23:38 +0100

    initial commit on quarkus app

---
+++ src/main/java/org/sec/quarkus/GreetingResource.java
@@ -0,0 +1,29 @@
+package org.sec.quarkus;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class GreetingResource {
+    @ConfigProperty
+    String greeting;
+
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello Quarkus";
+    }
+
+    @GET
+    @Path("/fr")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String greetings(){
+        return greeting + "From Quarkus";
+    }
+
+}
